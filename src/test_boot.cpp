0e359452b5a95aadddc66f998debd98bc09fb345
zhangxiaogang <zhangxiaogang@xiaomi.com>
2017-06-17 14:30:26 +0800

    add rmse

---
+++ src/test_boot.cpp
@@ -0,0 +1,4 @@
+//
+// Created by zhangxg on 17/6/17.
+//
+
--- src/tools.cpp
+++ src/tools.cpp
@@ -12,9 +12,31 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+//    rmse << (-1, -1, -1, -1);  // incorrect, no bracket
+    rmse << 0, 0, 0, 0;
+
+    //check the inputs validation
+    if (estimations.size() <= 0 && estimations.size() != ground_truth.size()) {
+        cout << "(warning) the input for rmse calculation is incorrect " << endl;
+        return rmse;
+    }
+
+    // calculate sum
+//    VectorXd residual(4);
+//    residual << (0, 0, 0, 0);
+    for (int i=0; i < estimations.size(); ++i) {
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+
+    // calculate the suqare root
+    rmse = rmse/estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
