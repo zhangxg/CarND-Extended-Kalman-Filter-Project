cc5949b96153a8d385b7c535e3c5118e1121c8f3
zhangxg <dlzhangxg@gmail.com>
2019-02-11 22:00:55 +0800

    first commit

--- src/experiments/01_file_reading/file_reading.cpp
+++ src/experiments/01_file_reading/file_reading.cpp
@@ -6,19 +6,19 @@
 #include <vector>
 
 #include "../../Eigen/Dense"
-#include "../../measurement_package.h"
 
-//using namespace std;
+#include "../../FusionEKF.h"
+#include "../../tools.h"
+#include "../../measurement_package.h"
 
 using Eigen::VectorXd;
 
-int main() {
-
+std::vector<MeasurementPackage> readMeasurements() {
   std::ifstream in("/Users/zhangxg/work/gitrepo/uda-sd/CarND-Extended-Kalman-Filter-Project/data/obj_pose-laser-radar-synthetic-input.txt");
   std::string line;
   std::string type;
   std::vector<MeasurementPackage> measurements;
-  float timestamp;
+  long long timestamp;
   float x_groundtruth, y_groundtruth, vx_groundtruth, vy_groundtruth;
   float yaw_groundtruth, yawrate_groundtruth;
   float rho_measured, phi_measured, rhodot_measured;
@@ -35,13 +35,14 @@ int main() {
       meas_package.sensor_type_ = MeasurementPackage::RADAR;
       meas_package.raw_measurements_ = VectorXd(3);
       meas_package.raw_measurements_ << rho_measured, phi_measured, rhodot_measured;
-
     } else if (type == "L") {
       iss >> x_measured;
       iss >> y_measured;
       meas_package.sensor_type_ = MeasurementPackage::LASER;
       meas_package.raw_measurements_ = VectorXd(2);
       meas_package.raw_measurements_ << x_measured, y_measured;
+    } else {
+      throw "Found un-recognized sensor type";
     }
     iss >> timestamp;
     iss >> x_groundtruth;
@@ -57,10 +58,42 @@ int main() {
                                   yaw_groundtruth, yawrate_groundtruth;
     measurements.push_back(meas_package);
   }
+  return measurements;
+}
+
+
+int main() {
+  std::vector<MeasurementPackage> measurements;
+  measurements = readMeasurements();
+  FusionEKF fusionEKF;
+  Tools tools;
+  vector<VectorXd> estimations;
+  vector<VectorXd> ground_truths;
+
   std::cout << measurements.size() << std::endl;
   for (MeasurementPackage m : measurements) {
-    std::cout << m.ground_truth_ << "--";
-    std::cout << std::endl;
+    fusionEKF.ProcessMeasurement(m);
+
+    VectorXd estimate(4);
+    estimate << fusionEKF.ekf_.x_(0), 
+                fusionEKF.ekf_.x_(1),
+                fusionEKF.ekf_.x_(2),
+                fusionEKF.ekf_.x_(3);
+
+    estimations.push_back(estimate);
+
+    VectorXd ground_truth(4);
+    ground_truth << m.ground_truth_(0),
+                    m.ground_truth_(1), 
+                    m.ground_truth_(2), 
+                    m.ground_truth_(3);
+    ground_truths.push_back(ground_truth);
+
+    VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truths);
+
+    std::cout << RMSE << std::endl;
   }
+  std::cout << "Done Calculation." << std::endl;
+
   return 0;
 }
